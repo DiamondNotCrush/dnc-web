c9124552f435bc201a6e5b32f2a82a5f8788592d
Owen Dismuke <owen.dismuke@gmail.com>
2015-08-29 13:30:27 -0700

    Working db model. Can post.

--- server/server-config.js
+++ server/server-config.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var model = require('./ConnectionModel.js');
 
 var app = express();
 
@@ -10,5 +11,14 @@ app.use('/', express.static('./public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
+
+//Route for /connect
+app.post('/connect', model.addConnection);
+
+//Route for /view
+app.post('/view', model.getConnections);
+
+//Route for user
+app.post('/user', model.addUser);
 
 module.exports = app;
