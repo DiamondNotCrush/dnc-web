b0aaf917827ea1e6b339266d439d950d3f05d3c5
Owen Dismuke <owen.dismuke@gmail.com>
2015-09-02 13:35:09 -0700

    Library now displays in view.

--- public/login/login.js
+++ public/login/login.js
@@ -2,10 +2,10 @@ angular
   .module('app.login', [])
   .controller('loginController', ['$state', 'user', function($state, user){
     this.login = function(){
-      user.details.name = 'Owen';
-      user.details.ip = 'http://99.9.62.40';
+      user.details.username = 'Owen';
+      user.details.ip = '99.9.62.40';
       user.details.port = 3000;
-      user.details.id = 2;
+      user.details.id = 1;
       user.details.isAuthorized = true;
 
       $state.go('view');
--- public/view/viewService.js
+++ public/view/viewService.js
@@ -2,16 +2,6 @@ angular
   .module('service.view', [])
   .service('view', ['$resource', 'user', function($resource, user){
     return {
-      File: $resource('', null, {
-        connections: {
-          method: 'POST',
-          url: '/connect'
-        },
-        files: {
-          method: 'GET',
-          url: '/user/library/:id', //Could test encodeURI()
-          params: {id: user.details.id}
-        }
-      })
+      Files: $resource('/user/library/:id', {id: '@id'})
     };
   }]);
