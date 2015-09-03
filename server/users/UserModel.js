4753ea984b960169eb7aa143d9b551e1dadd0815
csgriffis <christophergriffis@gmail.com>
2015-09-02 21:05:30 -0400

    User login functionality added

--- server/users/UserModel.js
+++ server/users/UserModel.js
@@ -9,48 +9,20 @@ module.exports = function (sequelize) {
   var User = sequelize.define("Users", {
     username: Sequelize.STRING,
     email: Sequelize.STRING,
-    password: Sequelize.STRING,
-    salt: Sequelize.STRING
+    password: {
+      type: Sequelize.STRING,
+      set: function (val) {
+        var salt = bcrypt.genSaltSync(10);
+        var hash = bcrypt.hashSync(val, salt);
+
+        this.setDataValue("password", hash);
+      }
+    }
   }, {
     instanceMethods: {
-      setToken: function () {
-        var defer = Q.defer();
-        // only hash password if it has been modified or is new
-        if (!this.changed("password")) {
-          return;
-        }
-        //generate salt
-        bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
-          if (err) {
-            console.log("Error generating salt: ", err);
-          }
-          //hash password
-          bcrypt.hash(this.password, salt, null, function (err, hash) {
-            if (err) {
-              defer.reject(err);
-            } else {
-              defer
-            }
-            //Set password and salt  
-            this.password = hash;
-            this.salt = salt;
-          });
-        });
-
-        return this;
-      },
-
-      comparePasswords: function (candidatePassword, hashedPassword) {
-        var defer = Q.defer();
+      comparePasswords: function (candidatePassword) {
         var savedPassword = this.password;
-        bcrypt.compare(candidatePassword, hashedPassword, function (err, isMatch) {
-          if (err) {
-            defer.reject(err);
-          } else {
-            defer.resolve(isMatch);
-          }
-        });
-        return defer.promise;
+        return bcrypt.compareSync(candidatePassword, savedPassword);
       }
     }
   });
