343029d6419ca4ded0abfc476b834ae482b6f78a
csgriffis <christophergriffis@gmail.com>
2015-09-11 19:05:59 -0400

    Updated Acount Page UI

--- public/account/account.js
+++ public/account/account.js
@@ -1,7 +1,23 @@
 angular
   .module('app.account', [])
-  .controller('accountController', ['user', function(user){
+  .controller('accountController', ['user', 'auth', function(user, auth){
     var _this = this;
     _this.user = user.details;
     
+    //update password
+    _this.updatePassword = function(currentPassword, newPassword) {
+      auth.update({id: _this.user.id, password: currentPassword, newPassword: newPassword})
+        .$promise
+        .then(function(res) {
+          //do something with response
+        });
+    },
+    //update email
+    _this.updateEmail = function(password, newEmail) {
+      auth.update({id: _this.user.id, password: password, email: newEmail})
+        .$promise
+        .then(function(res) {
+          //do something with response
+        });
+    }
   }]);
--- server/server-config.js
+++ server/server-config.js
@@ -6,11 +6,11 @@ var app = express();
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize(process.env.MYSQL || 'mysql://root@localhost:3306/dnc', {logging: false});
 
-// //Call models to set associations
+//Call models to set associations
 var Users = require("./users/UserModel")(sequelize);
 var Connections = require("./connections/ConnectionModel")(sequelize);
 
-// //Setup database tables
+//Setup database tables
 Users.User.hasMany(Connections.Connections, {as: 'UserId'});
 sequelize.sync();
 
